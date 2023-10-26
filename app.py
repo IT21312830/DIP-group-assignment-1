cf303d8af28edad4a508a67a956ab9df917b3c29
IT21311840 <99327764+IT21311840@users.noreply.github.com>
2023-10-26 15:34:03 +0530

    Saliha Rizan
    app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
-from PIL import Image, ImageOps, ImageFilter
+from PIL import Image, ImageOps, ImageFilter, ImageEnhance
 import os
 import io
 import base64
 
 app = Flask(__name__)
-
-# Configuration for file uploads
 app.config["UPLOAD_FOLDER"] = "static/uploads"
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -16,32 +13,19 @@ def index():
     modified_image = None
 
     if request.method == "POST":
-        # Check if a file was uploaded
         if "file" not in request.files:
             return redirect(request.url)
-
-        # retrieves the uploaded file from the POST request.
+        
         file = request.files["file"]
-
-        # checks if the uploaded file has a filename. If not redirect
         if file.filename == "":
             return redirect(request.url)
-
+        
         if file:
-            # Save the original image
             file_path = f"{app.config['UPLOAD_FOLDER']}/original.jpg"
             file.save(file_path)
-
-            # Open the original image
             original_image = file_path
-
+            
     return render_template("index.html", original_image=original_image, modified_image=modified_image)
-
-
-@app.route("/upload", methods=["POST"])
-def upload():
-    return redirect(url_for("index"))
-
 
 @app.route("/manipulate", methods=["POST"])
 def manipulate():
@@ -49,13 +33,11 @@ def manipulate():
     rotation = int(request.form["rotation"])
     crop_coords = request.form["crop"].split(',')
     flip = "flip" in request.form
-    smoothing = "smoothing" in request.form
-    embossing = "embossing" in request.form
+    sharpen_factor = float(request.form["sharpen"])
+    blur_factor = float(request.form["blur"])
+    edge_detection = "edge_detection" in request.form
 
-    # Open the original image
     original_image = Image.open(f"{app.config['UPLOAD_FOLDER']}/original.jpg")
-
-    # Perform manipulations
     modified_image = original_image.copy()
 
     if color_change == "bw":
@@ -72,23 +54,30 @@ def manipulate():
     if flip:
         modified_image = ImageOps.invert(modified_image)
 
-    if smoothing:
-        modified_image = modified_image.filter(ImageFilter.SMOOTH)
+    if edge_detection:
+        modified_image = modified_image.filter(ImageFilter.FIND_EDGES)
 
-    if embossing:
-        modified_image = modified_image.filter(ImageFilter.EMBOSS)
+    if sharpen_factor != 1.0:
+        sharpness = ImageEnhance.Sharpness(modified_image)
+        modified_image = sharpness.enhance(sharpen_factor)
 
-    # Save the modified image to a BytesIO object
+    if blur_factor > 1.0:
+        modified_image = modified_image.filter(ImageFilter.GaussianBlur(radius=blur_factor))
+
+    contrast_factor = float(request.form["contrast"])
+    contrast_enhancer = ImageEnhance.Contrast(modified_image)
+    modified_image = contrast_enhancer.enhance(contrast_factor)
+
+    brightness_factor = float(request.form["brightness"])
+    brightness_enhancer = ImageEnhance.Brightness(modified_image)
+    modified_image = brightness_enhancer.enhance(brightness_factor)
+
     output = io.BytesIO()
     modified_image.save(output, format="JPEG")
     output.seek(0)
 
-    return render_template(
-        "index.html",
-        original_image=f"/{app.config['UPLOAD_FOLDER']}/original.jpg",
-        modified_image=f"data:image/jpeg;base64,{base64.b64encode(output.read()).decode()}",
-    )
-
+    return render_template("index.html", original_image=f"/{app.config['UPLOAD_FOLDER']}/original.jpg",
+                           modified_image=f"data:image/jpeg;base64,{base64.b64encode(output.read()).decode()}")
 
 if __name__ == "__main__":
     app.run(debug=True)
